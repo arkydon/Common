974ca49ba023f3dd669560800a20350db84ac5fc
Ejz <ejz@ya.ru>
2013-06-13 19:10:18 +0400

    Update pen.php

--- squat/pen.php
+++ squat/pen.php
@@ -1,16 +1,16 @@
 <?php
     function CLI_squat_go() {
-		// var_dump(Whois::lookup('pen.org'));
-		// return;
-		while(true):
-		$lexeme = array(
-		    'buy' => '[buy|kupi|kupiti|pokupay]',
-		    'pen' => '[pen|pencil|ru4ka|ruchka]',
-		    'tld' => '[ru|org|com|net]'
-		);
-		$default = include(OTHER_ROOT . '/generator.db');
-		$domain = generator('@rotate({[@buy|(0.3)]}{@pen}).@tld', $default + $lexeme);
-		if(WhoIs::lookup($domain)) ;
-		else echo $domain, chr(10);
-		endwhile;
-	}
+        // var_dump(Whois::lookup('pen.org'));
+        // return;
+        while(true):
+        $lexeme = array(
+            'buy' => '[buy|kupi|kupiti|pokupay]',
+            'pen' => '[pen|pencil|ru4ka|ruchka]',
+            'tld' => '[ru|org|com|net]'
+        );
+        $default = include(OTHER_ROOT . '/generator.db');
+        $domain = generator('@rotate({[@buy|(0.3)]}{@pen}).@tld', $default + $lexeme);
+        if(WhoIs::lookup($domain)) ;
+        else echo $domain, chr(10);
+        endwhile;
+    }
